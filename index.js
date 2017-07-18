f9ac02fe3e87efd79b96d72db58416fe21d68712
jeremylenz <jeremylenz@gmail.com>
2017-07-18 17:43:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  let newArray = kittens.slice()
+  newArray.push(name)
+  return newArray
+}
+
+function prependKitten(name) {
+  let newArray = kittens.slice()
+  newArray.unshift(name)
+  return newArray
+}
+
+function removeLastKitten() {
+  let newArray = kittens.slice()
+  newArray.pop()
+  return newArray
+}
+
+function removeFirstKitten() {
+  let newArray = kittens.slice()
+  newArray.shift()
+  return newArray
+}
